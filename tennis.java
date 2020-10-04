0cddea8d160144db0aba0fe4e8e92b096e4f29d8
Niya Xu <nyxu@Niyas-Mac-Mini.local>
2020-10-04 17:21:20 -0400

    Set controls

--- tennis.java
+++ tennis.java
@@ -4,28 +4,62 @@ import java.applet.Applet;
 import java.awt.Color;
 import java.awt.Graphics;
 
-public class Tennis extends Applet implements Runnable{
+public class Tennis extends Applet implements Runnable, KeyListener{
     final int WIDTH = 700, HEIGHT = 500;
+    Thread thread;
+
     public void init() {
         this.resize(WIDTH, HEIGHT);
+
+        this.addKeyListener(this);
+
+        thread = new Thread(this);
+        thread.start();
     }
+
     public void paint(Graphics g) {
         g.setColor(Color.black)
         g.fillReact(0, 0, WIDTH, HEIGHT);
     }
+
     public void update(Graphics g) {
         paint(g);
     }
+
     public void run() {
         for(;;){
+
+            repaint();
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
             
         }
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+
+        }
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            
+        }
+        
+    }
+
+    public void keyTyped(KeyEvent arg0) {
+        
     }
 }
 
